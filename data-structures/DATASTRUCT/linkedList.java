3e1e2669f49d2afed2ddee283af5229e73db3c41
Kevin Nguyen <xuannamkevin@gmail.com>
2019-02-01 14:57:17 -0500

    basic single linked list

--- data-structures/DATASTRUCT/linkedList.java
+++ data-structures/DATASTRUCT/linkedList.java
@@ -1,1 +1,35 @@
 package DATASTRUCT;
+
+class LinkedList {
+	Node head;
+
+	public void insert(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node lastNode = this.head;
+			while (lastNode.next != null) {
+				lastNode = lastNode.next;
+			}
+			lastNode.next = newNode;
+		}
+	}
+	public void printList() {
+		Node currentNode = this.head;
+		while(currentNode!= null) {
+			System.out.println(currentNode.data+" -> ");
+			currentNode=currentNode.next;
+			
+		}
+	}
+}
+
+class Node {
+	int data;
+	Node next;
+
+	Node(int initialData) {
+		data = initialData;
+	}
+}
--- data-structures/DATASTRUCT/main.java
+++ data-structures/DATASTRUCT/main.java
@@ -1,1 +1,13 @@
 package DATASTRUCT;
+public class Main {
+	public static void main (String[] args) {
+		System.out.println("---------Running Main.Java---------");
+		LinkedList list= new LinkedList();
+		list.insert(5);
+		list.insert(7);
+		list.insert(8);
+		list.printList();
+
+	
+	}
+}
